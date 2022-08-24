ed4725c26281543a6c5825979bd9626ef0b73f08
Monali Sharma <mona.chaudhari444@gmail.com>
2022-08-24 17:29:26 +0530

    All test cases are passed and successfully execute main method and also handle exceptions

--- src/main/java/com/jap/calculator/MathematicalCalculator.java
+++ src/main/java/com/jap/calculator/MathematicalCalculator.java
@@ -2,26 +2,32 @@ package com.jap.calculator;
 
 public class MathematicalCalculator {
 
-	public int add(int num1, int num2)
-	{	
-		return -1;
+	public int add(int num1, int num2) {
+		return num1 + num2;
 	}
-	public int subtract(int num1, int num2)
-	{	
-		return -2;
-	}
-	public int multiply(int num1, int num2)
-	{	
-		return -1;
-	}
-	public int divide(int num1, int num2)
-	{
-		//write the code and handle the ArithmeticException
-         return -1;
 
+	public int subtract(int num1, int num2) {
+		return num1 - num2;
 	}
-	public int modulo(int num1, int num2)
-	{	
-		return-1;
+
+	public int multiply(int num1, int num2) {
+		return num1 * num2;
+	}
+
+	public int divide(int num1, int num2) {
+		int result = 0;
+		//write the code and handle the ArithmeticException
+		try {
+			if (num2 != 0 || num2 < 0) {
+				result = num1 / num2;
+			}
+		} catch (ArithmeticException e) {
+			System.out.println(e);
+		}
+		return result;
+	}
+
+	public int modulo(int num1, int num2) {
+		return num1 % num2;
 	}
 }
--- src/main/java/com/jap/calculator/ScientificCalculator.java
+++ src/main/java/com/jap/calculator/ScientificCalculator.java
@@ -2,43 +2,77 @@ package com.jap.calculator;
 
 public class ScientificCalculator {
 
-	public double cielOfANumber(float num) throws CalculatorException
-	{
-		//check if num is equal to or less than 0 than throw the userdefined exception with proper message
-		//or else call ceil method of Math class and return the value
-
-		return 1;
-	}
-	
-	public double floorOfANumber(float num) throws CalculatorException
-	{
-		//check if num is equal to or less than 0 than throw the userdefined exception with proper message
-		//or else call floor method of Math class and return the value
-		return 1;
-	}
-	
-    public long power(int num1, int num2) throws CalculatorException {
-		//if num1 is less than num2 than throw the userdefined exception with proper message
-		//else use Math pow method and return the value
-
-		return -1;
-    }
-    
-    public double squareRoot(int num) throws CalculatorException
-    {
-		//check if num is equal to or less than 0 than throw the userdefined exception with proper message
-			//or else call sqrt method of Math class and return the value
-
-		return -1;
-    }
-
 	public static void main(String[] args) {
 		ScientificCalculator scientificCalculator = new ScientificCalculator();
 		try {
-			scientificCalculator.cielOfANumber(10);
+			System.out.println("Ceil of Number =" + scientificCalculator.cielOfANumber(10));
 		} catch (CalculatorException e) {
-			throw new RuntimeException(e);
+			System.out.println(e);
+			;
+		}
+		try {
+			System.out.println("Floor of Number =" + scientificCalculator.floorOfANumber(20));
+		} catch (CalculatorException e) {
+			System.out.println(e);
 		}
 		//Call all the methods one by one and surround them by try and respective catch blocks.
+		try {
+			System.out.println("Power of Number =" + scientificCalculator.power(5, 2));
+		} catch (CalculatorException e) {
+			System.out.println(e);
+		}
+		try {
+			System.out.println("Square root of Number =" + scientificCalculator.squareRoot(5));
+		} catch (CalculatorException e) {
+			System.out.println(e);
+		}
+
+		MathematicalCalculator mathematicalCalculator = new MathematicalCalculator();
+
+		System.out.println("Addition of integer number = " + mathematicalCalculator.add(10, 20));
+		System.out.println("Subtraction of integer number = " + mathematicalCalculator.subtract(50, 20));
+		System.out.println("Multiplication of integer number = " + mathematicalCalculator.multiply(10, 5));
+		System.out.println("Division of integer number = " + mathematicalCalculator.divide(10, -5));
+		System.out.println("Modulo of integer number = " + mathematicalCalculator.add(50, 20));
+	}
+
+	public double cielOfANumber(float num) throws CalculatorException {
+		//check if num is equal to or less than 0 than throw the userdefined exception with proper message
+		//or else call ceil method of Math class and return the value
+		if (num <= 0) {
+			throw new CalculatorException("Number should not be negative or zero");
+		} else {
+			return Math.ceil(num);
+		}
+	}
+	
+	public double floorOfANumber(float num) throws CalculatorException {
+		//check if num is equal to or less than 0 than throw the userdefined exception with proper message
+		//or else call floor method of Math class and return the value
+		if (num <= 0) {
+			throw new CalculatorException("Number should not be negative or zero");
+		} else {
+			return Math.floor(num);
+		}
+	}
+
+	public long power(int num1, int num2) throws CalculatorException {
+		//if num1 is less than num2 than throw the userdefined exception with proper message
+		//else use Math pow method and return the value
+		if (num1 < num2) {
+			throw new CalculatorException("Number1 should be greater than Number2");
+		} else {
+			return (long) Math.pow(num1, num2);
+		}
+	}
+
+    public double squareRoot(int num) throws CalculatorException {
+		//check if num is equal to or less than 0 than throw the userdefined exception with proper message
+		//or else call sqrt method of Math class and return the value
+		if (num <= 0) {
+			throw new CalculatorException("Number should not be negative or zero");
+		} else {
+			return Math.sqrt(num);
+		}
 	}
 }
